57d113a908dbd9ce49873afb83501acb8e5c7cc6
feeeeen4ik <feeeeen4ik1994@gmail.com>
2025-09-29 23:32:18 +0500

    project is ready!

--- Lesson 1 - Calculator/ViewController.swift
+++ Lesson 1 - Calculator/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func plus(_ sender: Any) {
+        guard let firstNumber = Int(firstNumber.text!), let secondNumber = Int(
+            secondNumber.text!) else {
+            result.text = "Введите 2 числа"
+            return
+        }
+        
+        result.text = "\(firstNumber + secondNumber)"
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        guard let firstNumber = Int(firstNumber.text!), let secondNumber = Int(
+            secondNumber.text!) else {
+            result.text = "Введите 2 числа"
+            return
+        }
+        
+        result.text = "\(firstNumber - secondNumber)"
     }
 
-
+    @IBAction func devide(_ sender: Any) {
+        guard let firstNumber = Int(firstNumber.text!), let secondNumber = Int(
+            secondNumber.text!) else {
+            result.text = "Введите 2 числа"
+            return
+        }
+        
+        if secondNumber == 0 {
+            result.text = "На ноль делить нельзя!"
+            return
+        }
+        
+        result.text = "\(firstNumber + secondNumber)"
+    }
+    
+    @IBAction func multiply(_ sender: Any) {
+        guard let firstNumber = Int(firstNumber.text!), let secondNumber = Int(
+            secondNumber.text!) else {
+            result.text = "Введите 2 числа"
+            return
+        }
+        
+        result.text = "\(firstNumber * secondNumber)"
+    }
+    
+    
+    
 }
 
